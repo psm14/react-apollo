8fafcb22ac4b265a0b326170a4d582cafba669cd
Patrick McLaughlin <pat.mclaughlin@mobiledefense.com>
2019-09-05 18:24:12 -0400

    Fix error with RN fast refresh

--- packages/hooks/src/utils/useBaseQuery.ts
+++ packages/hooks/src/utils/useBaseQuery.ts
@@ -43,10 +43,17 @@ export function useBaseQuery<TData = any, TVariables = OperationVariables>(
     memo
   );
 
-  useEffect(() => queryData.afterExecute({ lazy }), [result]);
+  useEffect(() => {
+    if (queryDataRef.current) {
+      queryData.afterExecute({ lazy });
+    }
+  }, [result]);
 
   useEffect(() => {
-    return () => queryData.cleanup();
+    return () => {
+      queryData.cleanup();
+      queryDataRef.current = undefined;
+    };
   }, []);
 
   return result;
